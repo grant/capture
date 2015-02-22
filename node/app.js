4968b4fc5795b35697dcb8e9db2bfda8e6cf7de7
aakagi <aakagi@purdue.edu>
2015-02-21 16:11:21 -0800

    gun

--- node/app.js
+++ node/app.js
@@ -9,7 +9,11 @@ var routes = require('./routes/index');
 
 var app = express();
 
+var gunApp = require("./gunApp");
+gunApp(app);
+
 // view engine setup
+console.log("the base directory is", __dirname);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -24,36 +28,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 app.set('port', process.env.PORT || 3000);
 console.log('go to: http://localhost:' + app.get('port'));
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
 
 module.exports = app;
--- node/public/js/main.js
+++ node/public/js/main.js
@@ -1,76 +1,42 @@
-var json = {
-  rects: [{
-    box: {
-      left: 0.0,
-      top: 0.0,
-      right: 1.0,
-      bottom: 1.0,
-    },
-    color: [255,255,255]
-  },{
-    box: {
-      left: 0.0,
-      top: 0.0,
-      right: 1,
-      bottom: 0.1,
-    },
-    color: [100, 30, 200],
-    text: 'NAV BAR'
-  }, {
-    box: {
-      left: 0.1,
-      top: 0.2,
-      right: 0.3,
-      bottom: 0.6,
-    },
-    color: [0, 150, 0],
-    text: 'hello there'
-  }, {
-    box: {
-      left: 0.4,
-      top: 0.2,
-      right: 0.6,
-      bottom: 0.6,
-    },
-    color: [0, 150, 0],
-    text: 'hello there'
-  }, {
-    box: {
-      left: 0.7,
-      top: 0.2,
-      right: 0.9,
-      bottom: 0.6,
-    },
-    color: [0, 150, 0],
-    text: 'hello there'
-  }],
-}
+var gun = Gun('http://localhost:3000/gun').load('testing').path('rects');
 
-var rgbString = function(rectIndex) {
-  return 'background-color: rgb(' + json.rects[rectIndex].color[0]+','+json.rects[rectIndex].color[1]+','+json.rects[rectIndex].color[2] + ');';
+gun.map(function(val, i) {
+  console.log(val, i);
+  //var elem = $('#mainBody').append('<div id="'+i+'" class="absolute" style="text-align: center;'+boxString(i)+rgbString(i)+'">'+json.rects[i].text+'</div>'); 
+  var elem = $('#' + i).length? $('#' + i) : $('<div id="'+i+'" class="absolute">' + val.text +'</div>').appendTo('#mainBody'); 
+  this.path('box').on(function(pos) {
+    var css = boxString(pos);
+    elem.css(css);
+  });
+  this.path('color').on(function(pos) {
+    var css = rgbString(pos);
+    elem.css(css);
+  });
+});
+
+var rgbString = function(color) {
+  return {'background-color': 'rgb(' + color.r+','+color.g+','+color.b + ')'};
 }
-var boxString = function(rectIndex) {
-  var box = json.rects[rectIndex].box;
+var boxString = function(box) {
   box.left*=100;
   box.top*=100;
   box.right*=100;
   box.bottom*=100;
 
-  return 'margin-left:'+box.left+'%;margin-top:'+box.top+'%;width:'+(box.right-box.left)+'%;height:'+(box.bottom-box.top)+'%;';
+  console.log(box);
+
+  return {'margin-left': box.left+'%', 'margin-top': box.top+'%', 'width': (box.right-box.left)+'%', 'height': (box.bottom-box.top)+'%'};
 }
 
-for (i=0; i<json.rects.length; i++) {
-  $('#mainBody').append('<div id="'+i+'" class="absolute" style="text-align: center;'+boxString(i)+rgbString(i)+'">'+json.rects[i].text+'</div>'); 
-}
-
-$('.absolute').click(function() {
+$(document).on('click', '.absolute', function() {
   var clickedId = $(this).attr('id');
-  var currentRgb = json.rects[clickedId].color; //rgb array
-  var currentText = json.rects[clickedId].text;
+  // var currentRgb = json.rects[clickedId].color; //rgb array
+  // var currentText = json.rects[clickedId].text;
 
   var newText = prompt("Change Text");
 
   if (newText) {
+    gun.path(clickedId).set({text: newText});
     $('#'+clickedId).text(newText);
   }
   // alert('clickedId: ' + clickedId + '\ncurrentRgb: ' + currentRgb + '\ncurrentText: ' + currentText);
