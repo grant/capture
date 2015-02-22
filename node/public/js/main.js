2d689c293e9deb082717cb3a53805d413e467fb1
aakagi <aakagi@purdue.edu>
2015-02-22 00:47:57 -0800

    back in business with firebase

--- node/public/js/main.js
+++ node/public/js/main.js
@@ -2,85 +2,123 @@
 
 var fire = new Firebase('https://shining-fire-1739.firebaseio.com/');
 
-var json = {
-  rects: {
-    0: {
-      box: {
-        left: 0.0,
-        top: 0.0,
-        right: 1.0,
-        bottom: 1.0
-      },
-      color: {
-        r: 255,
-        g: 255,
-        b: 255
-      }
-    },
-    1: {
-    box: {
-        left: 0.0,
-        top: 0.0,
-        right: 1,
-        bottom: 0.1
-      },
-      color: {
-        r: 100,
-        g: 30,
-        b: 200
-      }
-      // text: 'NAV BAR'
-    },
-    2: {
-      box: {
-        left: 0.1,
-        top: 0.2,
-        right: 0.3,
-        bottom: 0.6
-      },
-      color: {
-        r: 0,
-        g: 150,
-        b: 0
-      }
-      // text: 'hello there'
-    },
-    3: {
-      box: {
-        left: 0.4,
-        top: 0.2,
-        right: 0.6,
-        bottom: 0.6
-      },
-      color: {
-        r: 0,
-        g: 150,
-        b: 0
-      }
-      // text: 'hello there'
-    },
-    4: {
-      box: {
-        left: 0.7,
-        top: 0.2,
-        right: 0.9,
-        bottom: 0.6
-      },
-      color: {
-        r: 0,
-        g: 150,
-        b: 0
-      }
-      // text: 'hello there'
-    }
+// var json = {
+//   rects: {
+//     0: {
+//       box: {
+//         left: 0.0,
+//         top: 0.0,
+//         right: 1.0,
+//         bottom: 1.0
+//       },
+//       color: {
+//         r: 255,
+//         g: 255,
+//         b: 255
+//       }
+//     },
+//     1: {
+//     box: {
+//         left: 0.0,
+//         top: 0.0,
+//         right: 1,
+//         bottom: 0.1
+//       },
+//       color: {
+//         r: 100,
+//         g: 30,
+//         b: 200
+//       }
+//       // text: 'NAV BAR'
+//     },
+//     2: {
+//       box: {
+//         left: 0.1,
+//         top: 0.2,
+//         right: 0.3,
+//         bottom: 0.6
+//       },
+//       color: {
+//         r: 0,
+//         g: 150,
+//         b: 0
+//       }
+//       // text: 'hello there'
+//     },
+//     3: {
+//       box: {
+//         left: 0.4,
+//         top: 0.2,
+//         right: 0.6,
+//         bottom: 0.6
+//       },
+//       color: {
+//         r: 0,
+//         g: 150,
+//         b: 0
+//       }
+//       // text: 'hello there'
+//     },
+//     4: {
+//       box: {
+//         left: 0.7,
+//         top: 0.2,
+//         right: 0.9,
+//         bottom: 0.6
+//       },
+//       color: {
+//         r: 0,
+//         g: 150,
+//         b: 0
+//       }
+//       // text: 'hello there'
+//     }
+//   }
+// };
+
+// fire.set(json);
+
+var json;
+fire.child('rects').once('value', function(snap) {
+  json = snap.val();
+
+
+  var rgbString = function(rectIndex) {
+    return 'background-color: rgb(' + json[rectIndex].color.r+','+json[rectIndex].color.g+','+json[rectIndex].color.b+ ');';
   }
-};
 
-fire.set(json);
+  var boxString = function(rectIndex) {
+    var box = json[rectIndex].box;
+    box.left*=100;
+    box.top*=100;
+    box.right*=100;
+    box.bottom*=100;
 
-var temp = fire.child('rect/2/box/left');
+    return 'margin-left:'+box.left+'%;margin-top:'+box.top+'%;width:'+(box.right-box.left)+'%;height:'+(box.bottom-box.top)+'%;';
+  }
+  console.log(json);
+  $.each(json, function(i, val) {
+    $('#mainBody').append('<div id="'+i+'" class="absolute" style="text-align: center;'+boxString(i)+rgbString(i)+'">'+json[i].text+'</div>'); 
+  });
 
-console.log(temp);
+  // $('.absolute').click(function() {
+  //   var clickedId = $(this).attr('id');
+  //   var currentRgb = json.rects[clickedId].color; //rgb array
+  //   var currentText = json.rects[clickedId].text;
+
+  //   var newText = prompt("Change Text");
+
+  //   if (newText) {
+  //     $('#'+clickedId).text(newText);
+  //   }
+  //   // alert('clickedId: ' + clickedId + '\ncurrentRgb: ' + currentRgb + '\ncurrentText: ' + currentText);
+  // });
+});
+
+
+
+
+// console.log(temp);
 
 
 // myDataRef.on('child_added', function(snapshot) {
@@ -104,30 +142,32 @@ console.log(temp);
 // });
 
 
-var rgbString = function(color) {
-  return {'background-color': 'rgb(' + color.r+','+color.g+','+color.b + ')'};
-}
-var boxString = function(box) {
-  box.left*=100;
-  box.top*=100;
-  box.right*=100;
-  box.bottom*=100;
+// var rgbString = function(color) {
+//   return {'background-color': 'rgb(' + color.r+','+color.g+','+color.b + ')'};
+// }
+// var boxString = function(box) {
+//   box.left*=100;
+//   box.top*=100;
+//   box.right*=100;
+//   box.bottom*=100;
 
-  console.log(box);
+//   console.log(box);
 
-  return {'margin-left': box.left+'%', 'margin-top': box.top+'%', 'width': (box.right-box.left)+'%', 'height': (box.bottom-box.top)+'%'};
-}
+//   return {'margin-left': box.left+'%', 'margin-top': box.top+'%', 'width': (box.right-box.left)+'%', 'height': (box.bottom-box.top)+'%'};
+// }
 
-$(document).on('click', '.absolute', function() {
-  var clickedId = $(this).attr('id');
-  // var currentRgb = json.rects[clickedId].color; //rgb array
-  // var currentText = json.rects[clickedId].text;
+// $(document).on('click', '.absolute', function() {
+//   var clickedId = $(this).attr('id');
+//   // var currentRgb = json.rects[clickedId].color; //rgb array
+//   // var currentText = json.rects[clickedId].text;
 
-  var newText = prompt("Change Text");
+//   var newText = prompt("Change Text");
 
-  if (newText) {
-    gun.path(clickedId).set({text: newText});
-    $('#'+clickedId).text(newText);
-  }
-  // alert('clickedId: ' + clickedId + '\ncurrentRgb: ' + currentRgb + '\ncurrentText: ' + currentText);
-});
+//   if (newText) {
+//     gun.path(clickedId).set({text: newText});
+//     $('#'+clickedId).text(newText);
+//   }
+//   // alert('clickedId: ' + clickedId + '\ncurrentRgb: ' + currentRgb + '\ncurrentText: ' + currentText);
+// });
+
+
